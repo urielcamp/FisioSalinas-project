dd85a72a95884f64429926551d1c9673c08bd802
Uriel Campaña <urielcamp@gmail.com>
2024-07-22 14:38:59 -0300

    se implemento la seccion about, quedan cosas por mejorar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,31 @@ import React from 'react'
 const About = () => {
   return (
     <section id='about' className='about-Container'>
-      <article className='physicalTerapy-container'>
+
+      <article className='about'>
+
+        <div className='container-img'>
+
+          <img src="" alt="" />
+
+        </div>
+
+        <div className='container-textAbout'>
+          
+          <h2 className='title-about'>
+            Licenciado Richard Ferreira
+          </h2>
+
+          <p className='text-about'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati vero nihil quasi aspernatur soluta,
+            porro voluptatum aliquam natus consequatur culpa,
+            molestiae voluptates ab incidunt nulla at tenetur, reprehenderit nesciunt nisi!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, magni dolorem consequatur velit
+            beatae recusandae magnam culpa quisquam dolores eveniet laborum ea vitae quidem?
+            Id laborum quas aperiam praesentium. Labore.
+          </p>
+
+        </div>
 
       </article>
 
