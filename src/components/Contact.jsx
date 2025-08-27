b4ebdcce2d4898e9a635469a404fd6118d331b6b
Uriel Campaña <urielcamp@gmail.com>
2025-08-27 15:27:17 -0300

    modifique el diseno de la seccion de contacto, ahora el form esta sobre la izquierda y a la derecha hay un contenedor que va a llevar una lista con datos del dt y abajo las redes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,27 +2,44 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <section className='contact-container' id='contact'>
-      <div className='form-container'>
-        <h1 className='titleContact'>Contactanos</h1>
-        <form action="/submit_contact_form" method="post">
-          <label  htmlFor="nombre">Nombre completo:</label>
-          <input type="text" id="nombre" name="nombre" required />
-          
-          
-          <label htmlFor="telefono">Celular:</label>
-          <input type="tel" id="telefono" name="telefono" required />
-          
-          <label htmlFor="email">Correo:</label>
-          <input type="email" id="email" name="email" required />
-          
-          <label htmlFor="descripcion">Mensaje:</label>
-          <textarea id="descripcion" name="descripcion" rows="4" required></textarea>
-          
-          <button type="submit">Enviar</button>
-        </form>
+        <section className="contact-container" id="contact">
+      <div className="contact-content">
+        <div className="form-container">
+          <h1 className="titleContact">Contáctanos</h1>
+          <form action="/submit_contact_form" method="post">
+            <div className="name-row">
+              <input type="text" placeholder="Nombre" required />
+              <input type="text" placeholder="Apellido" required />
+            </div>
+            <input type="tel" id="telefono" name="telefono" placeholder="Celular" required />
+            <input type="email" id="email" placeholder="Correo" required />
+            <textarea id="descripcion" rows="2" placeholder="Descripción" required></textarea>
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+
+        <div className="list-container">
+
+          <div className="listContact-container">
+            <h2>Datos de contacto</h2>
+            <ul>
+              <li><strong>Teléfono:</strong> +598 99 123 456</li>
+              <li><strong>Email:</strong> contacto@fisioterapia.com</li>
+            </ul>
+          </div>
+        
+
+          <div className='redesContact-container'>
+              <a href="#" className='fa-brands fa-facebook'></a>
+              <a href="#" className='fa-brands fa-instagram'></a>
+              <a href="#" className='fa-brands fa-whatsapp'></a>
+              <a href="#" className='fa-solid fa-envelope'></a>
+          </div>
+        </div>
+
       </div>
     </section>
+
   );
 };
 
